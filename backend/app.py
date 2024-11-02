945577bad3dd7c450856aa535764f9cf6919fc8c
Marvinda0 <jesusm.garcialonso@gmail.com>
2024-11-02 17:13:05 -0400

    Cambiado el look de la homepage, twiiter

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ import re
 import time
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)  # Permitir solicitudes de CORS
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './static/css/style.css';
+import logo from './images/logo.jpeg';
+import xlogo from './images/xlogo.png';
 
 function App() {
     const [numFallecidos, setNumFallecidos] = useState(0);
@@ -20,23 +22,32 @@ function App() {
 
     return (
         <div className="App">
-            <nav className="navbar">
-    <img src="./images/logo.jpeg" alt="Logo de DANAinfo" className="navbar-logo" />
+  <nav className="navbar">
+  <a href="/">
+    <img src={logo} alt="Logo de DANAInfo" className="navbar-logo" />
+    </a>
+    <h1 className="navbar-brand">
+        <span className="navbar-title">DANAInfo</span>
+    </h1>
     <ul className="navbar-nav">
         <li className="nav-item"><a href="#">Inicio</a></li>
         <li className="nav-item"><a href="#">Desaparecidos</a></li>
         <li className="nav-item"><a href="#">Puntos de ayuda</a></li>
     </ul>
-</nav>
+    <a href="https://twitter.com/intent/tweet?text=Por%20favor%20compartid%20esta%20web%20para%20informar%20sobre%20la%20DANA" target="_blank" rel="noopener noreferrer">
+    <img src={xlogo} alt="Logo de X" className="navbar-xlogo" />
+    </a>
+  </nav>
 
             <header className="header">
-                <h2>DANAinfo</h2>
                 <p>Proyecto dedicado a proporcionar información sobre víctimas de desastres.</p>
             </header>
 
             <div className="stats">
                 <p><strong>Número de muertos:</strong> <span id="numMuertos">{numFallecidos}</span></p>
+                <p className="clarification-text">Cifras provisionales ofrecidas por el Gobierno español este sábado</p> 
                 <p><strong>Número de desaparecidos:</strong> <span id="numDesaparecidos">0</span></p>
+                <p className="clarification-text">Desaparecidos publicados en esta pagine web en concreto</p>
             </div>
 
             <footer className="footer">
